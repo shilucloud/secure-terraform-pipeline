0566a7337388bc078f70bfa90d2f7e1913ca9145
shilucloud <shilash112001@gmail.com>
2025-10-06 13:31:09 +0530

    update: separate cd.yml, shell.nix for cd workflow and using only s3 for s3 backend, no need of dynamodb

--- shell.nix
+++ shell.nix
@@ -7,49 +7,53 @@ let
 
   pkgs = import nixpkgs { config.allowUnfree = true; };
 
-  # Detect CI from env variable
+  # Detect CI and CD from env variables
   isCI = builtins.getEnv "IS_CI" == "true";
+  isCD = builtins.getEnv "IS_CD" == "true";
 in
 pkgs.mkShell {
   name = "secure-terraform-pipeline-shell";
   pure = true;
 
-  buildInputs = if isCI then
-    [ pkgs.go pkgs.checkov pkgs.conftest pkgs.terraform pkgs.terraform-local pkgs.awscli2]
-  else
-    [
-      pkgs.git
-      pkgs.awscli2
-      pkgs.go
-      pkgs.checkov
-      pkgs.conftest
-      pkgs.docker_28
-      pkgs.terraform
-      pkgs.act
-      pkgs.terraform-local
-    ];
+  buildInputs =
+    if isCD then
+      [ pkgs.terraform ]   # minimal for CD
+    else if isCI then
+      [ pkgs.go pkgs.checkov pkgs.conftest pkgs.terraform pkgs.terraform-local pkgs.awscli2 ]
+    else
+      [
+        pkgs.git
+        pkgs.awscli2
+        pkgs.go
+        pkgs.checkov
+        pkgs.conftest
+        pkgs.docker_28
+        pkgs.terraform
+        pkgs.terraform-local
+        pkgs.act
+      ];
 
   shellHook = ''
-  export IS_CI="${if isCI then "true" else "false"}"
-  
-  echo "======================================"
-  echo "Pinned nixpkgs dev shell loaded!"
-  export APP_ENV=development
-
-  if [ "$IS_CI" = "true" ]; then
-    echo "Running in CI mode (only go, checkov, conftest, terraform included)"
-  else
-    echo "Running locally (all dev tools included)"
-  fi
+    export IS_CI="${if isCI then "true" else "false"}"
+    export IS_CD="${if isCD then "true" else "false"}"
+    export APP_ENV=development
 
     echo "======================================"
-    echo "Checking tool versions..."
-    go version
-    checkov --version
-    conftest --version
-    terraform --version
+    if [ "$IS_CD" = "true" ]; then
+      echo "Running in CD mode (only Terraform + AWS CLI installed)"
+    elif [ "$IS_CI" = "true" ]; then
+      echo "Running in CI mode (CI tools installed)"
+    else
+      echo "Running locally (all dev tools installed)"
+    fi
+    echo "======================================"
 
-    if [ "$IS_CI" != "true" ]; then
+    echo "Checking tool versions..."
+    terraform --version
+    if [ "$IS_CD" != "true" ]; then
+      go version
+      checkov --version
+      conftest --version
       git --version
       aws --version
       docker --version || echo "Docker not available or requires sudo"
