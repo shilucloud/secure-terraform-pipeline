f937cc5d6c44d48ea9578301d1d03a7f17893e36
shilucloud <shilash112001@gmail.com>
2025-10-07 07:46:08 +0530

    update: shell.nix for driftctl

--- shell.nix
+++ shell.nix
@@ -8,8 +8,9 @@ let
   pkgs = import nixpkgs { config.allowUnfree = true; };
 
   # Detect CI and CD from env variables
-  isCI = builtins.getEnv "IS_CI" == "true";
-  isCD = builtins.getEnv "IS_CD" == "true";
+  isCI = builtins.getEnv "WORKFLOW" == "ci";
+  isCD = builtins.getEnv "WORKFLOW" == "cd";
+  isDriftCtl = builtins.getEnv "WORKFLOW" == "driftctl";
 in
 pkgs.mkShell {
   name = "secure-terraform-pipeline-shell";
@@ -17,7 +18,9 @@ pkgs.mkShell {
 
   buildInputs =
     if isCD then
-      [ pkgs.terraform ]   # minimal for CD
+      [ pkgs.terraform ] 
+    else if isDriftCtl then 
+      [ pkgs.driftctl pkgs.terraform ]
     else if isCI then
       [ pkgs.go pkgs.checkov pkgs.conftest pkgs.terraform pkgs.terraform-local pkgs.awscli2 ]
     else
@@ -37,13 +40,16 @@ pkgs.mkShell {
   shellHook = ''
     export IS_CI="${if isCI then "true" else "false"}"
     export IS_CD="${if isCD then "true" else "false"}"
+    export IS_DRIFTCTL="${if isDriftCtl then "true" else "false"}"
     export APP_ENV=development
 
     echo "======================================"
     if [ "$IS_CD" = "true" ]; then
-      echo "Running in CD mode (only Terraform + AWS CLI installed)"
+      echo "Running in CD mode (only Terraform )"
     elif [ "$IS_CI" = "true" ]; then
       echo "Running in CI mode (CI tools installed)"
+    elif [ "$IS_DRIFTCTL" = "true" ]; then 
+      echo "Running in DriftCTL mode (terraform and checkov)"
     else
       echo "Running locally (all dev tools installed)"
     fi
@@ -51,7 +57,7 @@ pkgs.mkShell {
 
     echo "Checking tool versions..."
     terraform --version
-    if [ "$IS_CD" != "true" ]; then
+    if [ "$IS_CI" == "true" ]; then
       go version
       checkov --version
       conftest --version
@@ -59,7 +65,22 @@ pkgs.mkShell {
       aws --version
       docker --version || echo "Docker not available or requires sudo"
       act --version
-    fi
-    echo "======================================"
+
+    elif [ "$IS_DRIFTCTL" == "true" ]; then
+      driftctl version 
+
+    elif [ "$IS_CD" == "true" ]; then
+      echo "===================================="
+    else 
+      go version
+      checkov --version
+      conftest --version
+      git --version
+      aws --version
+      docker --version || echo "Docker not available or requires sudo"
+      act --version
+      driftctl version
+    fi 
+    echo "======================================="
   '';
 }
